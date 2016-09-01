952c57137a7a50ba8a857cc62c21d6a0be001e10
Johana <jhoana-ortiz@hotmail.com>
2016-08-31 20:12:32 -0500

    hola

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,20 +37,10 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { :host => :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
+   config.action_view.raise_on_missing_translations = true
+   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
 
-   
-    config.action_mailer.smtp_settings = {
-          address: "smtp.gmail.com",
-          port: 587,
-          authentication: "plain",
-          enable_starttls_auto: true,
-          user_name: "johana0745@gmail.com",
-          password: "johana9209"
-
-  }
 
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -77,16 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options =   { :host => 'https://new-home-book.herokuapp.com/' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: "johana0745@gmail.com",
-      password: "johana9209"
 
-}
 end
 
