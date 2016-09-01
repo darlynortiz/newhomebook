2ac8b688013586b6f82c6b4490d3bd799d050fc4
Johana <jhoana-ortiz@hotmail.com>
2016-08-31 20:19:59 -0500

    234

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+   config.action_mailer.default_url_options = { host: 'new-home-book.herokuapp.com' }
+   config.action_mailer.delivery_method = :letter_opener
+
 
 
 end
